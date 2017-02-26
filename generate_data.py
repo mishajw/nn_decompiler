ba03d2978438e2f41f1c3bf8d438fb13b4d38c5b
Misha Wagner <mishajw@gmail.com>
2017-02-26 20:04:10 +0000

    Write out generated functions

--- generate_data.py
+++ generate_data.py
@@ -3,6 +3,8 @@
 import argparse
 import generate_assembly_functions
 import generate_source_functions
+import re
+import os
 
 parser = argparse.ArgumentParser(description = "Generate data for translating \
         assembly to source code using the Linux Kernel")
@@ -14,14 +16,28 @@ parser.add_argument("--output_directory", type=str, default="output")
 def main():
     args = parser.parse_args()
 
+    output_directory = args.output_directory
+
     assembly_functions = generate_assembly_functions.generate(args.object_file)
     source_functions = generate_source_functions.generate(args.source_directory)
 
-    for function in assembly_functions:
-        print(function.name)
+    def strip_for_file_name(original):
+        return re.sub("[^0-9A-Za-z]+", "", original)
 
+    print("Writing out assembly functions")
+    for function in assembly_functions:
+        file_name = strip_for_file_name(function.name) + "_assembly"
+        with open(os.path.join(output_directory, file_name), 'w') as f:
+            for line in function.lines:
+                f.write(line + "\n")
+
+    print("Writing out source functions")
     for function in source_functions:
-        print(function.name)
+        file_name = strip_for_file_name(function.name) + "_source"
+
+        with open(os.path.join(output_directory, file_name), 'w') as f:
+            for line in function.body:
+                f.write(line)
 
 
 if __name__ == "__main__":
