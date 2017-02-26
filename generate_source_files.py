5c33e88438dd825610e86af863691eb90c676e08
Misha Wagner <mishajw@gmail.com>
2017-02-26 19:22:17 +0000

    Implement simple function parser

--- generate_source_files.py
+++ generate_source_files.py
@@ -2,14 +2,86 @@
 
 import os
 
+import sys
+sys.setrecursionlimit(20000)
+
+
+class NotHeader(Exception):
+    pass
+
 
 def generate(source_directory):
+    all_functions = [f for l in get_all_functions(source_directory) for f in l]
+
+    for function in all_functions:
+        print(function)
+
+
+def get_all_functions(source_directory):
     for file in get_source_files(source_directory):
-        functions = get_functions_from_source(file)
+        print("Getting from file %s" % file)
+        yield get_functions_from_source(file)
 
 
 def get_functions_from_source(file_name):
-    return []
+    with open(file_name, 'r', encoding="ISO-8859-1") as file:
+        while True:
+            try:
+                function = get_function(get_character_generator(file))
+
+                if function:
+                    yield function
+
+            except StopIteration:
+                return
+
+
+def get_function(file):
+    def clear_to_new_line():
+        character = next(file)
+
+        if character != "\n":
+            clear_to_new_line()
+
+    def get_header():
+        character = next(file)
+
+        if character == "#":
+            clear_to_new_line()
+            raise NotHeader
+
+        if character == ";":
+            raise NotHeader
+
+        if character == "{":
+            return ""
+
+        return character + get_header()
+
+    def get_rest_of_block(bracket_depth=0):
+        character = next(file)
+
+        if character == "{":
+            bracket_depth += 1
+
+        if character == "}":
+            if bracket_depth == 0:
+                return ""
+            else:
+                bracket_depth -= 1
+
+        return character + get_rest_of_block(bracket_depth)
+
+    try:
+        header = get_header()
+    except NotHeader:
+        return
+
+    body = get_rest_of_block()
+
+    print("Function with %d header and %d body" % (len(header), len(body)))
+
+    return "%s{%s}" % (header, body)
 
 
 def get_source_files(source_directory):
@@ -18,3 +90,8 @@ def get_source_files(source_directory):
             if file.endswith(".h") or file.endswith(".c"):
                 yield os.path.join(root, file)
 
+
+def get_character_generator(file):
+    for line in file:
+        for character in line:
+            yield character
