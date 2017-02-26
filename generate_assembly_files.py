cbb249498f14a07b42dadab97ee07ed8d827502c
Misha Wagner <mishajw@gmail.com>
2017-02-26 16:35:20 +0000

    Add simple function generation

--- generate_assembly_files.py
+++ generate_assembly_files.py
@@ -1,5 +1,45 @@
 #!/usr/bin/env python
 
-def generate():
-    pass
+import subprocess
+import re
+import sys
 
+
+class AssemblyFunction:
+    def __init__(self, name, lines):
+        self.name = name
+        self.lines = lines
+
+
+def generate(object_file):
+    process = subprocess.Popen(
+        ["objdump", "-d", object_file],
+        stdout=subprocess.PIPE)
+
+    while True:
+        function = get_function(process.stdout)
+
+        if function:
+            print("%s has %d lines" % (function.name, len(function.lines)))
+
+
+def get_function(object_dump):
+    function_header_line = next(object_dump).rstrip().decode()
+
+    function_header_regex = \
+        re.search("^[0-9a-z]+ <([0-9A-Za-z_]+)>:", function_header_line)
+
+    if not function_header_regex:
+        sys.stderr.write("Couldn't find function header in %s\n" % function_header_line)
+        return
+
+    function_name = function_header_regex.group(1)
+
+    def get_function_lines():
+        for line in object_dump:
+            if line.rstrip().decode() == "":
+                return
+            else:
+                yield line.rstrip().decode()
+
+    return AssemblyFunction(function_name, list(get_function_lines()))
--- generate_data.py
+++ generate_data.py
@@ -6,14 +6,14 @@ import generate_source_files
 
 parser = argparse.ArgumentParser(description = "Generate data for translating \
         assembly to source code using the Linux Kernel")
-parser.add_argument("--object_file", type=str)
+parser.add_argument("--object_file", type=str, required=True)
 parser.add_argument("--output_directory", type=str, default="output")
 
 
 def main():
     args = parser.parse_args()
 
-    generate_assembly_files.generate()
+    generate_assembly_files.generate(args.object_file)
     generate_source_files.generate()
 
 
