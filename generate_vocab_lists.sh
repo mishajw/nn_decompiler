d1a7d13cddeecdeb3c263165c92cabbdd3e1d009
Misha Wagner <mishajw@gmail.com>
2017-04-17 10:59:06 +0100

    Add script for creating vocabulary lists of output/asm output/cpp directories

---
+++ generate_vocab_lists.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+get_words() {
+  source_directory=$1
+
+  # `cat` every file
+  find $source_directory -type f | xargs cat | \
+    # Print all words
+    grep -Eo '\w+|[^A-Za-z0-9_ ]+' | \
+    # Get frequencies
+    sort | uniq -c | sort -hr
+}
+
+echo "Creating asm vocab"
+get_words output/asm > output/asm_vocab.txt
+
+echo "Creating cpp vocab"
+get_words output/cpp > output/cpp_vocab.txt
+
+echo "Done"
+
